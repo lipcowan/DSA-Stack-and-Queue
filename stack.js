8db5b23595edc952b5c79f7039d248b151547af5
Justin Foster <jfoster91@gmail.com>
2020-09-25 11:35:04 -0400

    Set up stack with linked list

--- stack.js
+++ stack.js
@@ -5,37 +5,6 @@ class _Node {
     }
 }
 
-class Array {
-    constructor() {
-        this.length = 0;
-        this._capacity = 0;
-        this.ptr = memory.allocate(this.length);
-    }
-
-    push(value) {
-        if (this.length >= this._capacity) {
-            this._resize((this.length + 1) * Array.SIZE_RATIO);
-        }
-
-        memory.set(this.ptr + this.length, value);
-        this.length++;
-    }
-
-    _resize(size) {
-        const oldPtr = this.ptr;
-        this.ptr = memory.allocate(size);
-        if (this.ptr === null) {
-            throw new Error('Out of memory');
-        }
-        memory.copy(this.ptr, oldPtr, this.length);
-        memory.free(oldPtr);
-        this._capacity = size;
-    }
-}
-
-Array.SIZE_RATIO = 3;
-
-
 class LinkedList {
     constructor() {
         this.head = null;
@@ -43,6 +12,18 @@ class LinkedList {
 
     insertFirst(item) {
         this.head = new _Node(item, this.head);
+    }
+    
+    insertLast(item) {
+    if (this.head === null) {
+        this.insertFirst(item);
+    } else {
+        let tempNode = this.head;
+        while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+        }
+        tempNode.next = new _Node(item, null);
+    }
     }
 
     remove(item){ 
@@ -59,16 +40,12 @@ class LinkedList {
 class Stack {
     constructor() {
         this.top = null;
+        this.list = new LinkedList()
     }
 
     push(data) {
-        if (this.top === null) {
-            this.top = new _Node(data, null);
-            return this.top;
-        }
-
-        const node = new _Node(data, this.top);
-        this.top = node;
+        this.list.insertLast(data);
+        return this.top;
     }
 
     pop() {
@@ -79,7 +56,7 @@ class Stack {
 }
 
 
-const starTrek = [];
+const starTrek = new Stack();
 
 const main = () => {
     starTrek.push('Kirk');
